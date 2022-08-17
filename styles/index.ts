0574bb2235785a9e05bc2d8f9b411522bdd79c11
jeffubayi <jeffubayi@gmail.com>
2022-08-17 16:20:57 +0300

    add tailwindcsss & flowbite ui

---
+++ styles/index.ts
@@ -0,0 +1,6 @@
+import { CustomFlowbiteTheme } from "flowbite-react/lib/esm/components/Flowbite/FlowbiteTheme";
+
+export const theme: CustomFlowbiteTheme = {
+  
+  
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	content: [
+		'./node_modules/flowbite-react/**/*.js',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: 'class',
+	plugins: [require("flowbite/plugin")],
+}
