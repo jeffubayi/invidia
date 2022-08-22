ab42c28ad8bb7b43f298f5eef0c7f90e1fe647b2
jeffubayi <jeffubayi@gmail.com>
2022-08-22 12:52:20 +0300

    configure next image module

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['flowbite.com'],
+    domains: ['flowbite.com','https://aaepbxpivppmvuaemajn.supabase.co'],
   },
 }
 
