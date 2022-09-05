50308aac497414e440de162bde13a9a2bd94ada8
jeffubayi <jeffubayi@gmail.com>
2022-09-05 07:46:09 +0300

    add image url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['flowbite.com','https://aaepbxpivppmvuaemajn.supabase.co','https://assets-global.website-files.com',"https://i.imgur.com"],
+    domains: ['flowbite.com','https://aaepbxpivppmvuaemajn.supabase.co','https://assets-global.website-files.com',"https://i.imgur.com","https://encrypted-tbn0.gstatic.com"],
   },
 }
 
